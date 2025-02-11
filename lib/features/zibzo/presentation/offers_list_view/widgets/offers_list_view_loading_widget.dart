55b28acd859147b66120015fd116e061f74ba05f
Youshah <mohdyoushah@gmail.com>
2025-02-12 03:21:57 +0530

    Page Transition implemented

---
+++ lib/features/zibzo/presentation/offers_list_view/widgets/offers_list_view_loading_widget.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:zibzo/features/zibzo/presentation/widgets/custom_shimmer.dart';
+
+class CartLoadingWidget extends StatelessWidget {
+  const CartLoadingWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+        child: SingleChildScrollView(
+          physics: NeverScrollableScrollPhysics(),
+          child: Column(
+            children: const [
+              SizedBox(height: 16),
+              CustomShimmer(height: 250, width: double.infinity),
+              SizedBox(
+                height: 20,
+              ),
+              CustomShimmer(height: 250, width: double.infinity),
+              SizedBox(
+                height: 20,
+              ),
+              CustomShimmer(height: 250, width: double.infinity),
+              SizedBox(
+                height: 20,
+              ),
+              CustomShimmer(height: 250, width: double.infinity),
+              SizedBox(
+                height: 20,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
